11719f7a3bed6656cbb388d35331350ff4d19487
fuadnafiz98 <fuadnafiz98@gmail.com>
2021-08-12 03:33:40 +0600

    some plugins added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,10 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar"),
+  ],
 };
